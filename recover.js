87e164724106e9ef9e0f5c0dd8070fdc7b4b9501
Alexandre BERTRAND <lxndr.bertrand@gmail.com>
2018-04-26 11:16:11 +0200

    Create recover.js

---
+++ recover.js
@@ -0,0 +1,8 @@
+const recover = (prms, onSuccess = ((res) => res), onCatch = ((res) => res)) => {
+	return new Promise((resolve) => {
+		prms
+			.then(successValue => resolve(onSuccess(successValue)))
+			.catch(catchValue => resolve(onCatch(catchValue)))
+	})
+	
+}
